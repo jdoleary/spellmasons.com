2ad8887beada2a5835071d72c3dea683da9af5ab
Jordan O'Leary <jdoleary@gmail.com>
2021-04-25 09:17:42 -0400

    Basic site with social links

---
+++ src/App.svelte
@@ -0,0 +1,46 @@
+<script>
+  import SocialLinks from "./SocialLinks.svelte";
+</script>
+
+<main>
+  <h1>Spellmason</h1>
+  <p>
+    A multiplayer, strategy card game that lets you combine small spells into
+    larger, epic spells to decimate your enemies
+  </p>
+  <p>
+    You play a Spellmason, who hasn't finished wizard school yet, and so must
+    make up their own spells!
+  </p>
+  <p>
+    Say that you're surrounded, the enemies are closing in. You cast "swap" to
+    change places with one on the outside. Then you cast "Area of Effect",
+    "Damage", and "Resurrect", killing all the enemies who surrounded you a
+    moment before and resurrecting them to join your faction. Now the tables
+    have turned and your small army will charge into battle against the
+    now-fearful scraps of the enemy horde.
+  </p>
+  <SocialLinks />
+</main>
+
+<style>
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
+
+  h1 {
+    color: #3858c0;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+</style>
---
+++ src/SocialLinks.svelte
@@ -0,0 +1,29 @@
+<script>
+  import InstagramLogo from "./InstagramLogo.svelte";
+</script>
+
+<div id="links">
+  <InstagramLogo />
+  <a
+    href="https://twitter.com/SpellmasonGame?ref_src=twsrc%5Etfw"
+    class="twitter-follow-button"
+    data-size="large"
+    data-show-screen-name="false"
+    data-show-count="false">Follow @SpellmasonGame</a
+  ><script
+    async
+    src="https://platform.twitter.com/widgets.js"
+    charset="utf-8"></script>
+</div>
+
+<style>
+  #links {
+    display: flex;
+    /* flex-direction: column; */
+    justify-content: center;
+    align-items: center;
+  }
+  #links > * {
+    margin: 8px;
+  }
+</style>
