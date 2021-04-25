5126820aefab2652611efeb803ee0eed7b66a49d
Jordan O'Leary <jdoleary@gmail.com>
2021-04-25 09:46:00 -0400

    content: Add Twitch logo
    css: Adjust styling and hover over labels

--- src/SocialLinks.svelte
+++ src/SocialLinks.svelte
@@ -1,5 +1,7 @@
 <script>
   import InstagramLogo from "./InstagramLogo.svelte";
+  import TwitchTvLogo from "./TwitchTVLogo.svelte";
+  import YoutubeLink from "./YoutubeLink.svelte";
 </script>
 
 <div id="links">
@@ -14,6 +16,9 @@
     async
     src="https://platform.twitter.com/widgets.js"
     charset="utf-8"></script>
+
+  <TwitchTvLogo />
+  <!-- <YoutubeLink /> -->
 </div>
 
 <style>
---
+++ src/YoutubeLink.svelte
@@ -0,0 +1,5 @@
+<iframe
+  src="https://www.youtube.com/embed/?listType=user_uploads&list=YOURCHANNELNAME"
+  width="480"
+  height="400"
+/>
